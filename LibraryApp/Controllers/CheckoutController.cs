b8297902bfe67215e71b84beed5ab4d785ddf710
Yan P Aung <yan.pye.aung@gmail.com>
2020-06-26 20:53:16 -0700

    Add create transaction method with Braintree, first Successful purchase!!!, Woot!!!

--- LibraryApp/Controllers/CheckoutController.cs
+++ LibraryApp/Controllers/CheckoutController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Braintree;
 using LibraryApp.Data.Services;
 using LibraryApp.Data.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -10,19 +11,19 @@ namespace LibraryApp.Controllers
 {
     public class CheckoutController : Controller
     {
-        private readonly IBookService _courseService;
+        private readonly IBookService _bookService;
         private readonly IBraintreeService _braintreeService;
 
-        public CheckoutController(IBookService courseService, IBraintreeService braintreeService)
+        public CheckoutController(IBookService bookService, IBraintreeService braintreeService)
         {
-            _courseService = courseService;
+            _bookService = bookService;
             _braintreeService = braintreeService;
         }
 
         public IActionResult Purchase(Guid id)
         {
 
-            var book = _courseService.GetById(id);
+            var book = _bookService.GetById(id);
             if (book == null) return NotFound();
 
             var gateway = _braintreeService.GetGateway();
@@ -41,6 +42,33 @@ namespace LibraryApp.Controllers
             };
 
             return View(data);
+        }
+
+        public IActionResult Create(BookPurchaseVM model)
+        {
+            var gateway = _braintreeService.GetGateway();
+            var book = _bookService.GetById(model.Id);
+
+            var request = new TransactionRequest
+            {
+                Amount = Convert.ToDecimal(book.Price),
+                PaymentMethodNonce = model.Nonce,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+
+            if (result.IsSuccess())
+            {
+                return View("Success");
+            }
+            else
+            {
+                return View("Failure");
+            }
         }
     }
 }
