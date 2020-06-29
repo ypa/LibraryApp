40b2a2a100bada53c6b65742794569255427aca4
Yan P Aung <yan.pye.aung@gmail.com>
2020-06-28 20:43:56 -0700

    Add Stripe dashboard with balance and transactions controller, View and ViewModel

---
+++ LibraryApp/Controllers/StripeDashboardController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryApp.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+
+namespace LibraryApp.Controllers
+{
+    public class StripeDashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            var response = new StripeDashboardVM();
+
+            var balanceService = new BalanceService();
+            var balanceResult = balanceService.Get();
+
+            response.Balance = balanceResult;
+
+            var transactionService = new BalanceTransactionService();
+            var transactionResult = transactionService.List().ToList();
+
+            response.Transactions = transactionResult;
+            
+            return View(response);
+        }
+    }
+}
---
+++ LibraryApp/Data/ViewModels/StripeDashboardVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Stripe;
+
+namespace LibraryApp.Data.ViewModels
+{
+    public class StripeDashboardVM
+    {
+        public Balance Balance { get; set; }
+
+        public List<BalanceTransaction> Transactions { get; set; }
+    }
+}
